4584d699f9cfecb45512f4b335db54b500aace84
bahrus <anderson.bruce.b@gmail.com>
2018-07-27 11:54:01 -0400

    typo

--- bra-ket.js
+++ bra-ket.js
@@ -40,7 +40,7 @@ export function BraKetMixin(superClass) {
             else {
                 this.appendChild(clonedNode);
             }
-            this.setAttribue("shadowed", true);
+            this.setAttribute("shadowed", true);
         }
     };
 }
--- bra-ket.ts
+++ bra-ket.ts
@@ -44,7 +44,7 @@ export function BraKetMixin(superClass) {
             }else{
                 this.appendChild(clonedNode);
             }
-            this.setAttribue("shadowed", true);
+            this.setAttribute("shadowed", true);
         }
     }
 }
