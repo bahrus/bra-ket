06bc8fa543b775ebc2087cf7ce444dc99864db6e
bahrus <anderson.bruce.b@gmail.com>
2018-09-13 13:15:57 -0400

    update refs, make mixin typescript compatible

--- bra-ket.ts
+++ bra-ket.ts
@@ -1,19 +1,19 @@
 import {loadTemplate} from 'templ-mount/first-templ.js';
 
-
+type Constructor<T = {}> = new (...args: any[]) => T;
 
 export function lispToSnakeCase(s: string) {
     return s.split('-').join('_');
 }
-export function BraKetMixin(superClass) {
+export function BraKetMixin<TBase extends Constructor<HTMLElement>>(superClass: TBase) {
     return class extends superClass {
         static get is() { return 'bra-ket'; }
         get tn() {
             return this.tagName.toLowerCase();
         }
         looksLike() { }
-        _ce;
-        get dynamicSlots(): string[] {
+        _ce!: any;
+        get dynamicSlots(): string[] | null {
             return null;
         }
         get CE() {
@@ -34,12 +34,12 @@ export function BraKetMixin(superClass) {
             const tn = this.looksLike() || this.tn;
             if (!this.CE._template[tn]) {
                 const templateId = lispToSnakeCase(tn) + '_template';
-                this.CE._template[tn] = self[lispToSnakeCase(tn) + '_template'];
+                this.CE._template[tn] = (<any>self)[lispToSnakeCase(tn) + '_template'];
             }
             const clonedNode = this.CE._template[tn].content.cloneNode(true) as DocumentFragment;
             this.customizeClone(clonedNode);
             if(!noShadow){
-                this.shadowRoot.appendChild(clonedNode);
+                this.shadowRoot!.appendChild(clonedNode);
                 this.initShadowRoot();
             }else{
                 this.appendChild(clonedNode);
@@ -59,14 +59,11 @@ export class BraKet extends BraKetMixin(HTMLElement){
 }
 
 
-
-
-
 export function initCE(tagName: string, cls: any, basePath: string, sharedTemplateTagName?: string) {
     if (customElements.get(tagName)) return;
     const templateTagName = sharedTemplateTagName || tagName;
     const templateID = lispToSnakeCase(templateTagName) + '_template';
-    let template = self[templateID] as HTMLTemplateElement;
+    let template = (<any>self)[templateID] as HTMLTemplateElement;
     if (!template) {
         template = document.createElement('template');
         template.id = templateID;
